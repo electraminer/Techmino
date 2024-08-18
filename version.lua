4e9fc673e28b30a1449487d11bb7dcb46e649c75
Electra <arthurlschell@gmail.com>
2024-08-18 00:40:25 -0400

    Updated ver num

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 return {
     ["apkCode"]=1722,
     ["code"]=1722,
-    ["string"]="V0.17.22 KOSv1.0 / TE:Cv0.1",
-    ["room"]="ver A-14 KOSv1.0 / TE:Cv0.1",
+    ["string"]="V0.17.22 KOSv1.1 / TE:Cv1.0",
+    ["room"]="ver A-14 KOSv1.1 / TE:Cv1.0",
     ["name"]="暂停 Break",
 }
